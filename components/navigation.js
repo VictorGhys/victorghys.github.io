f9344cac170efa429212342b57f23b2acc71eb9d
Victor Ghys <victor.ghys@student.howest.be>
2022-08-22 18:21:38 +0200

    added little dice, bold letters

--- components/navigation.js
+++ components/navigation.js
@@ -8,7 +8,7 @@ class Navigation extends HTMLElement {
 		<nav id="nav">
 			<ul class="links">
 				<li><a href="index.html">Projects</a></li>
-				<li><a href="jams.html">Jams</a></li>
+				<li><a href="jams.html">Game Jams</a></li>
 				<li><a href="art.html">Art</a></li>
 				<li><a href="Victor Ghys CV.pdf" target="_blank" rel="noopener noreferrer">Resume (CV)</a></li>
 				<!-- <li><a href="generic.html">Generic Page</a></li> -->
